28e142d7667c18578d42c146ae1adb26ad4acbf9
Everly <66821749+everlyy@users.noreply.github.com>
2024-04-10 22:31:05 +0200

    Add UX_MAX to types.h

--- evlang/include/evlang/types.h
+++ evlang/include/evlang/types.h
@@ -19,4 +19,9 @@ typedef const char* cstr;
 #define NULL ((void*)0)
 #endif // NULL
 
+#define U8_MAX  (1 << 8)
+#define U16_MAX (1 << 16)
+#define U32_MAX (1 << 32)
+#define U32_MAX (1 << 32)
+
 #endif // _EVLANG_TYPES_H
