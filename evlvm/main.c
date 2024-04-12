fab2d7f797f7ebafebb3cf4c2020cd201832f726
Everly <66821749+everlyy@users.noreply.github.com>
2024-04-12 16:14:10 +0200

    evlvm: Get program from command-line arguments

--- evlvm/main.c
+++ evlvm/main.c
@@ -4,9 +4,14 @@
 #include <stdio.h>
 #include "vm.h"
 
-int main(void) {
-    cstr filename = "a.evpgm";
-    FILE* file = fopen(filename, "rb");
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        fprintf(stderr, "Not enough arguments.\n");
+        fprintf(stderr, "Usage: %s <file.evpgm>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* file = fopen(argv[1], "rb");
     ASSERT(file != NULL);
 
     EVPGM program = evpgm_read_from_file(file);
